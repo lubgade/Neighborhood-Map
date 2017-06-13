0a723e355e58a584dae620762043e42c4d5b24ca
lubgade <lubgade@yahoo.com>
2017-06-13 12:04:46 -0400

    integrate filter by name

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@
 function viewModel(){
     var self = this;
     var map;
+    var initialLat = 39.8282;
+    var initialLng = -98.5795;
     var place = '';
     var newLocation;
-    var filterLoc;
     var place_id;
     var largeInfowindow = new google.maps.InfoWindow({maxwidth: 300});
     var cors_anywhere_url = 'https://cors-anywhere.herokuapp.com/';
@@ -13,19 +14,20 @@ function viewModel(){
       // Create a new blank array for all the listing markers.
     var markers = [];
     this.showDiv = ko.observable(false);
+    this.showFilterDiv = ko.observable(false);
     this.searchLocation = ko.observable();
-    //console.log(this.searchLocation());
     this.filterLocation = ko.observable();
     this.restaurantList = ko.observableArray([]);
+    this.filteredList = ko.observableArray([]);
 
     var restaurant = function(data){
         this.name = data.name;
-        this.address = data.location.address + "<br>" + data.location.city +
-                       " " + data.location.zipcode;
+        this.address = data.location.address;
         this.rating = data.user_rating.aggregate_rating;
         this.menuUrl = data.menu_url;
         this.lat = parseFloat(data.location.latitude);
         this.lng = parseFloat(data.location.longitude);
+        this.thumbnail = data.thumb;
     }
 
 
@@ -34,7 +36,7 @@ function viewModel(){
         console.log('Initmap');
     //  creates a new map - only center and zoom are required.
         map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 39.8282, lng: -98.5795},
+        center: {lat: initialLat, lng: initialLng},
         zoom: 4,
         scrollwheel: false,
         zoomControl: true
@@ -52,6 +54,9 @@ function viewModel(){
         console.log(newLocation);
 
         newLocation.addListener('place_changed', function(){
+                var center = new google.maps.LatLng(initialLat, initialLng);
+                map.panTo(center);
+                map.setZoom(4);
                 place = newLocation.getPlace();
                 self.searchLocation(place);
                 console.log(self.searchLocation);
@@ -60,23 +65,17 @@ function viewModel(){
                 self.showDiv(false);
                 markers = [];
                 self.restaurantList.removeAll();
-
+                self.filteredList.removeAll();
                 });
 
         }; //end of initialize
 
-        function hideMarkers() {
-            for (var i = 0; i < markers.length; i++) {
-                markers[i].marker.setMap(null);
-        }
-      }
-
 
     this.processSearchLocation = function(formElement){
-        console.log(formElement["searchText"].value);
+        //console.log(formElement["searchText"].value);
         var geocoder = new google.maps.Geocoder();
         var place_name = formElement["searchText"].value;
-        console.log(place_name);
+        //console.log(place_name);
         if(place_name == ''){
             window.alert('Please enter a state');
         }
@@ -111,42 +110,33 @@ function viewModel(){
     function getZomatoCityId(place_name, newLat, newLng){
         var zomatoUrl = "https://developers.zomato.com/api/v2.1/" +
                         "cities?q=";
-                    $.ajax({
-                        type: 'GET',
-                        url: cors_anywhere_url + zomatoUrl + place_name +
-                        "&lat=" + newLat + "&lon=" + newLng + "&count=1",
-                        headers: {'user_key': '26ce1af09de13709ce7601f27ae5e14d'},
-
-                                }).done(function(response){
-                                    //console.log(response);
-                            place_id = response.location_suggestions[0].id;
-                            //console.log(place_id);
-                            getZomatoRestaurants(place_id);
-                                }).fail(function(){
-                                    console.log("error getting place id");
-                    });
+        $.ajax({
+            type: 'GET',
+            url: cors_anywhere_url + zomatoUrl + place_name +
+                 "&lat=" + newLat + "&lon=" + newLng + "&count=1",
+            headers: {'user_key': '26ce1af09de13709ce7601f27ae5e14d'},
+            }).done(function(response){
+                //console.log(response);
+                place_id = response.location_suggestions[0].id;
+                //console.log(place_id);
+                getZomatoRestaurants(place_id);
+            }).fail(function(){
+                console.log("error getting place id");
+        });
     }
 
 
     this.filterResults = function(formElement){
-        if(formElement["filterText"].value == ''){
-            window.alert('please enter a city');
-        }
-        else{
+        self.showFilterDiv(true);
+        showMarker(self.filterLocation());
 
-                    }
     }
 
 
-
-    this.goToMarker = function(clickedRestaurant){
-        console.log(clickedRestaurant);
-        var name = clickedRestaurant.name;
+    function showMarker(name){
         console.log(name);
         for(var i=0;i<markers.length;i++){
-            console.log(markers[i].marker.title);
             if(name == markers[i].marker.title){
-                console.log('if true');
                 map.panTo(markers[i].marker.position);
                 map.setZoom(14);
                 populateInfoWindow(markers[i].marker,largeInfowindow,
@@ -157,6 +147,21 @@ function viewModel(){
     }
 
 
+    this.goToMarker = function(clickedRestaurant){
+        //console.log(clickedRestaurant);
+        var name = clickedRestaurant.name;
+        //console.log(name);
+        showMarker(name);
+    }
+
+
+    function hideMarkers() {
+            for (var i = 0; i < markers.length; i++) {
+                markers[i].marker.setMap(null);
+        }
+      }
+
+
     function showRestaurants(data){
         console.log(data);
         for(var i=0;i<data.results_shown;i++){
@@ -164,9 +169,73 @@ function viewModel(){
             self.restaurantList.push(new restaurant(r));
             createMarker(r);
                 }
+            self.filteredList(self.restaurantList.slice(0));
 
     } //end of showRestaurants
 
+
+    function filterLoc(){
+        var list = [];
+                for(var i=0;i<self.restaurantList().length;i++){
+                    list.push({label:self.restaurantList()[i].name,
+                               address:self.restaurantList()[i].address,});
+                }
+
+                $('#filter_text').autocomplete({
+                source: list,
+
+                select: function(event,ui){
+                    var text = ui.item.label;
+                    console.log(text);
+                    self.filterLocation(text);
+
+                },
+                change: function(event, ui){
+                    if(ui.item != null){
+                    var text = ui.item.value;
+                    console.log(text);
+                    for(var i=0;i<list.length;i++){
+                        console.log(list[i].label);
+                        if(text == list[i].label){
+                            self.filteredList.removeAll();
+                            self.filteredList.push({name:list[i].label,
+                            address:list[i].address});
+                            //break;
+                        }
+                    }
+                    console.log(self.filteredList());
+
+                    }
+                    else{
+                        window.alert('please select a name');
+                    }
+                    //self.showDiv(true);
+
+                }
+                });
+
+    }
+
+
+    this.clearFilter = function(){
+        largeInfowindow.close();
+        var center = map.getCenter();
+        var lat = self.searchLocation().geometry.location.lat();
+        console.log(lat);
+        var lng = self.searchLocation().geometry.location.lng();
+        console.log(lng);
+        var cityCenter = new google.maps.LatLng(lat, lng);
+        console.log(cityCenter);
+        self.filterLocation('');
+        console.log(self.restaurantList());
+        self.filteredList(self.restaurantList.slice(0));
+        console.log(self.filteredList());
+        map.panTo(cityCenter);
+        map.setZoom(10);
+        for(var i=0;i<markers.length;i++){
+            markers[i].marker.setMap(map);
+        }
+    }
 
 
 function getZomatoRestaurants(place_id){
@@ -180,13 +249,12 @@ function getZomatoRestaurants(place_id){
             }).done(function(response){
                 //console.log(response);
                 showRestaurants(response);
+                filterLoc();
             }).fail(function(){
                 console.log("error getting restaurants");
 
         });
     } //end of getYelpReviews
-
-
 
 
     function createMarker(r){
@@ -207,9 +275,10 @@ function getZomatoRestaurants(place_id){
           });
 
         var contentString = '<div id="infoWindow">' +
+                            '<img src=' + r.thumb + ' alt="image">' +
                             '<h2>' + r.name + '</h2>' +
                             '<p>' + r.location.address + '</p>' +
-                            '<p class="rating">' +
+                            '<p class="rating">Rating: ' +
                             r.user_rating.aggregate_rating + '</p>' +
                             '<a href=' + r.menu_url + '>' + 'Menu Url</a>'
 
